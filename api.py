1969684e5b3b9b8bb60b8317c1d643ec21b1d344
underabloodysky <valenzuelahoracioe@gmail.com>
2023-01-28 22:59:04 -0300

    Adding comment to API file

--- api.py
+++ api.py
@@ -9,31 +9,32 @@
 
 # Our server will form a single node in our blockchain network. 
 
-from textwrap import dedent
-from time import time
 from uuid import uuid4
-from flask import Flask
-import blockchain
+from flask import Flask, jsonify
+from blockchain import Blockchain
 
 # Instantiate our Node
 app = Flask(__name__)
 
-# Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
+# Generate a globally unique address for this node. Create a random name for our node
+node_identifier = str(uuid4()).replace('-', '') 
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['GET']) 
+# Create the /mine endpoint, which is a GET request
 def mine():
     return "We'll mine a new Block"
   
-@app.route('/transactions/new', methods=['POST'])
+@app.route('/transactions/new', methods=['POST']) 
+#Create the /transactions/new endpoint, which is a POST request, since we’ll be sending data to it
 def new_transaction():
     return "We'll add a new transaction"
 
 @app.route('/chain', methods=['GET'])
+#Create the /chain endpoint, which returns the full Blockchain
 def full_chain():
     response = {
         'chain': blockchain.chain,
@@ -41,5 +42,12 @@ def full_chain():
     }
     return jsonify(response), 200
 
+# Runs the server on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+    
+    
+   
+    Line 32–38: 
